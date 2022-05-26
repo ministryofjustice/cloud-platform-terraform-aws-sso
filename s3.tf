31f0895876c60ba288381fb9f9bbbe30fcec5611
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2022-05-26 16:57:31 +0100

    add S3 policy

---
+++ s3.tf
@@ -0,0 +1,42 @@
+data "aws_iam_policy_document" "s3_for_github" {
+
+  statement {
+    sid    = "AllowListDescribe"
+    effect = "Allow"
+    actions = [
+      "s3:GetBucketAcl",
+      "s3:GetBucketLocation",
+      "s3:ListAllMyBuckets"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "AllowGetOwn"
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:ListBucketVersions"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringLike"
+      variable = "aws:PrincipalTag/GithubTeam"
+      values   = ["*:$${s3:ExistingObjectTag/GithubTeam}:*"]
+    }
+  }
+
+}
+
+resource "aws_iam_policy" "s3_for_github" {
+  policy = data.aws_iam_policy_document.s3_for_github.json
+  name   = "s3-for-github"
+  tags = {
+    GithubTeam = "webops"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "s3_for_github" {
+  role       = aws_iam_role.github_access.name
+  policy_arn = aws_iam_policy.s3_for_github.arn
+}
