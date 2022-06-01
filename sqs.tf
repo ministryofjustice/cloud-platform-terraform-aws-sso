a39b9f9c10801ba379d515e4870237635aedd3fd
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2022-06-01 08:25:12 +0100

    SQS policy

---
+++ sqs.tf
@@ -0,0 +1,42 @@
+data "aws_iam_policy_document" "sqs_for_github" {
+
+  statement {
+    sid    = "AllowListDescribe"
+    effect = "Allow"
+    actions = [
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:ListQueues"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "AllowSendRecvOwn"
+    effect = "Allow"
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:SendMessage"
+    ]
+    resources = ["*"]
+    condition {
+      test     = "StringLike"
+      variable = "aws:PrincipalTag/GithubTeam"
+      values   = ["*:$${aws:ResourceTag/GithubTeam}:*"]
+    }
+  }
+
+}
+
+resource "aws_iam_policy" "sqs_for_github" {
+  policy = data.aws_iam_policy_document.sqs_for_github.json
+  name   = "sqs-for-github"
+  tags = {
+    GithubTeam = "webops"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_for_github" {
+  role       = aws_iam_role.github_access.name
+  policy_arn = aws_iam_policy.sqs_for_github.arn
+}
