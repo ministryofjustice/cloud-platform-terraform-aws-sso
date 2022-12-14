28e1b5f3b72a72406a21964e731215d1d15d7fff
jakemulley <jakemulley@users.noreply.github.com>
2022-12-14 13:19:56 +0000

    Refactor to meet standardised Terraform structure

--- examples/main.tf
+++ examples/main.tf
@@ -6,6 +6,5 @@ provider "aws" {
 module "sso" {
   source = "../"
 
-  aws_account_id = "1234567890"
   auth0_tenant_domain = "test.eu.auth0.com"
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "aws_account_id" {
-  description = "The AWS Account numeric ID"
-  type        = string
-}
-
 variable "aws_callback_url" {
   description = "AWS SSO callback URL"
   type        = string
