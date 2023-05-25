eb5eb87b65d20e2a79886cde29884a61b1e2edf3
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-05-25 16:43:03 +0100

    Allow api-gateway:GET without ABAC and split policy

--- api-gateway.tf
+++ api-gateway.tf
@@ -3,11 +3,13 @@ data "aws_iam_policy_document" "api_gateway_for_github" {
     sid       = "AllowAPIGatewayGetOwn"
     effect    = "Allow"
     actions   = ["apigateway:GET"]
-    resources = ["*"]
-    condition {
-      test     = "StringLike"
-      variable = "aws:PrincipalTag/GithubTeam"
-      values   = ["*:$${aws:ResourceTag/GithubTeam}:*"]
-    }
+    resources = [
+				"arn:aws:apigateway:eu-west-2::/account",
+				"arn:aws:apigateway:eu-west-2::/restapis",
+				"arn:aws:apigateway:eu-west-2::/apis/*",
+				"arn:aws:apigateway:eu-west-2::/domainnames",
+				"arn:aws:apigateway:eu-west-2::/vpclinks",
+				"arn:aws:apigateway:eu-west-2::/apis"
+			]
   }
 }
