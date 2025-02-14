3a37bbe67e039afe9acd5a671828ed742883ed84
sj-williams <steve.williams@digital.justice.gov.uk>
2025-02-14 10:28:20 +0000

    feat: 🎸 update sso for filter teams api in SAML JS
    set axios deps for api call, add secret for storing api key

--- add-github-teams-to-aws-saml.js
+++ add-github-teams-to-aws-saml.js
@@ -1,10 +1,14 @@
+const axios = require('axios');
+
 exports.onExecutePostLogin = async (event, api) => {
   if (event.connection.name === "github") {
     const samlProvider = event.secrets.AWS_SAML_PROVIDER_NAME;
     const awsAccount = event.secrets.AWS_ACCOUNT_ID;
+    const filterApiKey = event.secrets.FILTER_API_KEY;
     const rolePrefix = "arn:aws:iam::" + awsAccount;
     const role = "access-via-github";
     const samlIdP = rolePrefix + ":saml-provider/" + samlProvider;
+    const filterUrl = "https://github-teams-filter.apps.live.cloud-platform.service.justice.gov.uk/filter-teams"
 
     const git_teams = event.user.user_metadata["gh_teams"].map((t) =>
       t.replace("github:", ""),
@@ -12,24 +16,42 @@ exports.onExecutePostLogin = async (event, api) => {
 
     const filteredTeams = git_teams.filter((t) => t != "all-org-members");
 
-    api.user.GithubTeam = ":" + filteredTeams.join(":") + ":";
-    api.user.awsRoleSession = event.user.nickname;
-    api.user.awsTagKeys = ["GithubTeam"];
-    api.user.awsRole = rolePrefix + ":role/" + role + "," + samlIdP;
+    const teamsString = ":" + filteredTeams.join(":") + ":";
+    try {
+      const response = await axios.post(filterUrl, 
+      { teams: teamsString }, 
+      {
+        headers: {
+          "X-API-Key": filterApiKey,
+          "Content-Type": "application/json"
+        }
+      });
 
-    api.samlResponse.setAttribute(
-      "https://aws.amazon.com/SAML/Attributes/Role",
-      rolePrefix + ":role/" + role + "," + samlIdP,
-    );
+      const result = response.data;
+      const filteredTeamsString = result["filtered_teams"];
 
-    api.samlResponse.setAttribute(
-      "https://aws.amazon.com/SAML/Attributes/RoleSessionName",
-      event.user.nickname,
-    );
+      api.user.GithubTeam = filteredTeamsString;
+      api.user.awsRoleSession = event.user.nickname;
+      api.user.awsTagKeys = ["GithubTeam"];
+      api.user.awsRole = rolePrefix + ":role/" + role + "," + samlIdP;
 
-    api.samlResponse.setAttribute(
-      "https://aws.amazon.com/SAML/Attributes/PrincipalTag:GithubTeam",
-      ":" + filteredTeams.join(":") + ":",
-    );
+      api.samlResponse.setAttribute(
+        "https://aws.amazon.com/SAML/Attributes/Role",
+        rolePrefix + ":role/" + role + "," + samlIdP,
+      );
+
+      api.samlResponse.setAttribute(
+        "https://aws.amazon.com/SAML/Attributes/RoleSessionName",
+        event.user.nickname,
+      );
+
+      api.samlResponse.setAttribute(
+        "https://aws.amazon.com/SAML/Attributes/PrincipalTag:GithubTeam",
+        filteredTeamsString,
+      );
+    } catch (error) {
+      console.error("Error filtering teams:", error);
+      throw new Error("Failed to filter teams");
+    }
   }
 };
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "saml_login_page" {
   value = "https://${var.auth0_tenant_domain}/samlp/${auth0_client.saml.client_id}?connection=github"
 }
+
+output "github_teams_filter_api_key" {
+  value = aws_ssm_parameter.auth0_action_saml_mapping_filter_api_key.value
+  sensitive = true
+}
